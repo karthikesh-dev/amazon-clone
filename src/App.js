1b530866994d93f1e1dc9c9c67dd859fca2dc74d
Karthikesh <karthiksh9596@gmail.com>
2024-08-08 11:01:51 +0530

    React Props & React Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from './Layout';
+import Checkout from './Checkout';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='checkout' element={<Checkout />} />
+          </Route>
+        </Routes>
+      </Router>
+      {/* <Layout />
+      <Home /> */}
     </div>
   );
 }
